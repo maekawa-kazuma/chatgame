21f7e6eb8a97c654d03d7d6829188014a32dfd87
maekawa <kazumaekawa7218@gmail.com>
2021-01-01 20:37:57 +0900

    Update: devise関連のルーティング再設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   get 'rooms/index'
   root to: "users#index"
   resources :users, only: :new
